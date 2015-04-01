c78192046215f28ac005150f299eb1be22fbb45f
Eric Ito <eric.ito@gmail.com>
2015-03-31 23:08:34 -0700

    Initial Commit

---
+++ Bubbles/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Bubbles
+//
+//  Created by Eric Ito on 3/31/15.
+//  Copyright (c) 2015 Eric Ito. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
